7d0619d8540cd0b063bedca655f326539564a9f7
rashidansari-csbs <mi2004klk@gmail.com>
2026-06-18 18:45:57 +0530

    good

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: '**', // allows all https domains
+        hostname: '**', // allows all https domainsss
         pathname: '/**',
       },
       {
